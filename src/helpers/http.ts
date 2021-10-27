81ef8cbf320c2f91efd9099d3a0805ecc0218aed
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2021-10-27 10:15:18 +0900

    ✨ Add http helper

---
+++ src/helpers/http.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_HOST,
+  timeout: 30000,
+});
+
+instance.interceptors.request.use((config) => {
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      return response;
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+export default instance;
