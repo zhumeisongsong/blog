17726a5e78a8635b3e3c6d337ecddff83e289a4b
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-08-30 21:24:57 +0900

    :wrench: IsGithubActions check in next.config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,26 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repo = isGithubActions
+  ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
+  : "";
+const assetPrefix = isGithubActions ? `/${repo}/` : "";
+const basePath = isGithubActions ? `/${repo}` : "";
+
 const nextConfig = {
-  output: 'export',
- 
+  basePath,
+  assetPrefix,
+  output: "export",
+
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
- 
+
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
- 
+
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
