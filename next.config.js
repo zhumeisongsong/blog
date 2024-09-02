b30011d790b10295c968fb1b86602fd6c5f58c19
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-09-02 17:21:08 +0900

    :wrench: Next config

--- next.config.js
+++ next.config.js
@@ -2,15 +2,12 @@
  * @type {import('next').NextConfig}
  */
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repo = isGithubActions
-  ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
-  : "";
-const assetPrefix = isGithubActions ? `/${repo}/` : "";
-const basePath = isGithubActions ? `/${repo}` : "";
+const assetPrefix = isGithubActions ? `/blog/` : "";
+const basePath = isGithubActions ? `/blog` : "";
 
 const nextConfig = {
-  // basePath,
-  // assetPrefix,
+  basePath,
+  assetPrefix,
   output: "export",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
