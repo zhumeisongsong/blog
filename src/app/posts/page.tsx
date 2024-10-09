fc51d221d9199af617587d15341e036335ae7f89
zhumeisongsong <zhumeisongsong@gmail.com>
2024-10-09 14:19:18 +0900

    feat: ✨ add AllPosts page

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,24 @@
+import Container from "@/app/_components/container";
+import { Intro } from "@/app/_components/intro";
+import { Posts } from "@/app/_components/posts";
+import { getAllPosts } from "@/lib/api";
+
+export default function AllPosts() {
+  const allPosts = getAllPosts();
+
+  return (
+    <main>
+      <Container>
+        <Intro />
+        {allPosts.length > 0 && (
+          <>
+            <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
+              All Stories
+            </h2>
+            <Posts posts={allPosts} />
+          </>
+        )}
+      </Container>
+    </main>
+  );
+}
