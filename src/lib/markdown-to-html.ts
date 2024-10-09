d06a849658674b39a6812310e588cd30fa3605bb
zhumeisongsong <zhumeisongsong@gmail.com>
2024-10-09 21:22:47 +0900

    feat: ✨ add remark-gemoji

--- src/lib/markdown-to-html.ts
+++ src/lib/markdown-to-html.ts
@@ -1,9 +1,11 @@
 import { remark } from "remark";
+import remarkGemoji from 'remark-gemoji'
 import remarkHtml from "remark-html";
 import remarkGfm from "remark-gfm";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
+    .use(remarkGemoji)
     .use(remarkGfm)
     .use(remarkHtml)
     .process(markdown);
