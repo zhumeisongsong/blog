d4d494b14a370b45a1f90d97f7e87f4b9737e256
zhumeisongsong <zhumeisongsong@gmail.com>
2024-10-10 15:49:09 +0900

    fix: 🐛 fix code section layout broken by <!-- mermaid -->

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -25,7 +25,7 @@ export function PostBody({ content }: Props) {
 
   useEffect(() => {
     if (content) {
-      setArray(content.split("```"));
+      setArray(content.split("<!-- mermaid -->"));
     }
   }, [content, setArray]);
 
