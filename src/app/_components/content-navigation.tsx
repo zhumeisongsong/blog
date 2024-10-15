23cbb3215fdb807c50a21c04f143e9a79930e067
zhumeisongsong <zhumeisongsong@gmail.com>
2024-10-15 20:00:52 +0900

    refactor: ♻️ Enhance accessibility with ARIA attributes

--- src/app/_components/content-navigation.tsx
+++ src/app/_components/content-navigation.tsx
@@ -22,28 +22,30 @@ function extractHeadings(markdown: string): { level: number; text: string }[] {
 export function ContentNavigation({ content }: Props) {
   const headings = extractHeadings(content);
   return (
-    <div className="sticky top-0">
-      <div className="absolute w-80 right-0 pl-8 text-sm">
-        {headings.map((heading, index) =>
-          heading.level === 2 ? (
-            <Link
-              key={index}
-              href={`#${titleToHyphen(heading.text)}`}
-              className="text-stone-500 dark:text-stone-400 block opacity-80 hover:opacity-100 py-3"
-            >
-              {heading.text}
-            </Link>
-          ) : (
-            <Link
-              key={index}
-              href={`#${titleToHyphen(heading.text)}`}
-              className="text-stone-400 dark:text-stone-500 block opacity-80 hover:opacity-100 pl-4 py-1 border-l-2"
-            >
-              {heading.text}
-            </Link>
-          )
-        )}
-      </div>
+    <div className="sticky top-0 hidden lg:block">
+      <nav
+        aria-label="Table of Contents"
+        className="absolute w-80 right-0 pl-8 text-sm"
+      >
+        <ul role="list">
+          {headings.map((heading, index) => (
+            <li key={index}>
+              <Link
+                key={index}
+                href={`#${titleToHyphen(heading.text)}`}
+                className={`block opacity-80 hover:opacity-100 ${
+                  heading.level === 2
+                    ? "text-stone-600 dark:text-stone-400 py-3"
+                    : "text-stone-400 dark:text-stone-500 pl-4 py-1 border-l-2"
+                }`}
+                aria-level={heading.level}
+              >
+                {heading.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
