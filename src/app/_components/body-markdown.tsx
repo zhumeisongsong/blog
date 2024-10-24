5df59eeae30fb519efc48a14b398f0ab76d64a00
zhumeisongsong <zhumeisongsong@gmail.com>
2024-10-24 11:57:08 +0900

    feat: ✨ scroll to target element position

--- src/app/_components/body-markdown.tsx
+++ src/app/_components/body-markdown.tsx
@@ -12,11 +12,26 @@ type Props = {
 
 export function BodyMarkdown({ content }: Props) {
   const [html, setHtml] = useState<string>("");
+
   useEffect(() => {
     markdownToHtml(content).then((html) => {
       setHtml(addIdsToHeadings(html));
     });
   }, [content]);
+
+  useEffect(() => {
+    if (!html) {
+      return;
+    }
+    const elementId = window.location.hash.substring(1);
+    const element = document.getElementById(elementId);
+
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, [html]);
 
   return (
     <div
