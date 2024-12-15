15f32b34454bf6fec7cbdbf114a57aac10c9d797
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-15 15:45:00 +0900

    feat: ✨ add pkgRoot arg to createReleaseConfig

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,9 @@
 const { createReleaseConfig } = require("@zhumeisong/semantic-release-config");
 
-const srcRoot = `./`;
+const srcRoot = "./";
+const pkgRoot = "./";
 
 module.exports = createReleaseConfig({
-  srcRoot
+  srcRoot,
+  pkgRoot
 });
