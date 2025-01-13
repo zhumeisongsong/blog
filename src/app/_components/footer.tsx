57188361748fa83ad2f7b06bc0fef8f088b31ee1
zhumeisongsong <zhumeisongsong@gmail.com>
2025-01-13 14:30:08 +0900

    feat: ✨ Update connection list item order

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -25,6 +25,15 @@ export function Footer() {
           </div>
 
           <div className="flex gap-2">
+            <a
+              href="https://www.linkedin.com/in/zhumei-song-a9041a1bb"
+              target="_blank"
+            >
+              <img
+                alt="Linkedin"
+                src="https://img.shields.io/badge/linkdin-0a66c2.svg?&style=for-the-badge&logo=linkedin&logoColor=white"
+              />
+            </a>
             <a href="https://github.com/zhumeisongsong" target="_blank">
               <img
                 alt="GitHub"
@@ -36,16 +45,6 @@ export function Footer() {
               <img
                 alt="Blog"
                 src="https://img.shields.io/badge/Song's%20Blog-6c3485.svg?&style=for-the-badge&logo=Next.js&logoColor=white"
-              />
-            </a>
-
-            <a
-              href="https://www.linkedin.com/in/zhumei-song-a9041a1bb"
-              target="_blank"
-            >
-              <img
-                alt="Linkedin"
-                src="https://img.shields.io/badge/linkdin-0a66c2.svg?&style=for-the-badge&logo=linkedin&logoColor=white"
               />
             </a>
             {/* add cv */}
